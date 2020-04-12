5f1c89d7e44bd3fbc77817d19d62813501a3b296
rick_tan <rick_tan@qq.com>
2020-04-12 16:48:56 +0800

    Perfect business logic processing

--- actions.py
+++ actions.py
@@ -1,8 +1,8 @@
-from typing import Dict, Text, Any, List, Union
-
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
 from rasa_sdk.forms import FormAction
+from typing import Dict, Text, Any, List, Union
+
 
 class SalesForm(FormAction):
     """Collects sales information and adds it to the spreadsheet"""
@@ -13,12 +13,12 @@ class SalesForm(FormAction):
     @staticmethod
     def required_slots(tracker):
         return [
-            "job_function",
+            "job",
             "use_case",
             "budget",
-            "person_name",
+            "name",
             "company",
-            "business_email",
+            "email",
         ]
 
     def submit(
@@ -29,3 +29,31 @@ class SalesForm(FormAction):
     ) -> List[Dict]:
         dispatcher.utter_message("感谢您的联系，我们会尽快与您联系")
         return []
+
+    def slot_mappings(self) -> Dict[Text, Union[Dict, List[Dict]]]:
+        """A dictionary to map required slots to
+            - an extracted entity
+            - intent: value pairs
+            - a whole message
+            or a list of them, where a first match will be picked"""
+
+        return {
+            "job": [
+                self.from_text(intent="inform"),
+            ],
+            "use_case": [
+                self.from_text(intent="inform"),
+            ],
+            "budget": [
+                self.from_text(intent="inform"),
+            ],
+            "name": [
+                self.from_text(intent="inform"),
+            ],
+            "company": [
+                self.from_text(intent="inform"),
+            ],
+            "business_email": [
+                self.from_text(intent="inform"),
+            ],
+        }
